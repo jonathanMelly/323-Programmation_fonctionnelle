6120984083c7bd35b5f26c94542fafe1073469c5
Jonathan Melly <jonathan.melly@gmail.com>
2025-09-30 14:51:18 +0200

    feat(seq6): first draft of seq6

--- exos/diffit/Program_sans_todo.cs
+++ exos/diffit/Program_sans_todo.cs
@@ -25,13 +25,13 @@ if (!filesAreValid)
 }
 
 /// CHARGEMENT DES DONNÉES
-// TODO: 01 Charger le contenu texte du fichier A (indice: File.ReadAllLines...)
+
 string[] linesA = { "..." };
 
-// TODO: 02 Charger le contenu texte du fichier B (indice: File.ReadAllLines...)
+
 string[] linesB = { "..." };
 
-// TODO: 03 Vérifier que les fichier ont le même nombre de lignes
+
 if (true)
 {
     Console.WriteLine("Erreur: les fichiers n'ont pas le même nombre de ligne");
@@ -40,10 +40,7 @@ if (true)
 
 Console.WriteLine(">Fichiers chargés avec succés");
 
-// TODO: 04 Définir les fonctions de nettoyage
-// Une fonction de nettoyage reçoit un texte (une ligne de fichier) et renvoie cette même ligne adaptée
-// Il existe la fonction Replace sur les string...
-// Le caractère tabulation s’écrit \t
+/// Fonctions de nettoyage
 Func<string, string> cleanSpaces = text => text;
 Func<string, string> cleanTabs = text => text;
 Func<string, string> enforceCase = text => text;
@@ -60,40 +57,20 @@ bool ignoreTabs = Console.ReadLine() == "o";
 Console.Write("-Ignorer la casse [o/n]: ");
 bool ignoreCase = Console.ReadLine() == "o";
 
-// TODO:  05 Appliquer le nettoyage selon la demande utilisateur
 
-
-// TODO: 06 Créer et remplir une liste de LinesComparison à partir de linesA et linesB
+/// Calcul des comparaisons
 List<LinesComparison> comparisons = new();
 
-// TODO: 07 Sélectionner les lignes qui ont des différences
 var diffLines = new Liste<LinesComparison>();
 
-// TODO: 08 Afficher le nombre de lignes identiques et différentes entre les 2 fichiers
-
-// TODO: 09 Définir une fonction qui compte les différences (caractères différents) entre deux textes (sera utilisé pour les 2 lignes de A et B...)
-// Pour info/rappel, la fonction Zip (comme une fermeture éclair) permet d’associer deux listes.
-// Et pour info/rappel, un string est une liste de char...
-// Ainsi "12345".Zip("ABCDE", (a, b) => $"{a}{b}").ToList().ForEach(Console.Write);//1A2B3C4D5E
-// ATTENTION: zip ne prend que le nombre d’éléments minimum commun entre 2 listes...
-// Ceci implique une correction: en plus du nombre de différences, il faut ajouter la différence du nombre de caractères entre les deux...
 Func<LinesComparison, int> countVariations = _ => -1;
 
-// TODO: 10 Afficher pour chaque ligne différente, le nombre de variations
+
 
 /// Diff coloré
-// TODO: 11 Colorier les différences
-// Pour chaque ligne où il y a des différences:
-// On affiche ainsi:
-// Les lettres similaires sont en vert
-// Les lettres différentes sont en rouge (options entre[a/b])
-// On n’indique rien sur les caractères en plus ou en moins
+
 
 /// Chiffrement
-// TODO: 11 Créer une fonction qui chiffre le 1er fichier en décalant les caratères d’un nombre
-//saisi par l’utilisateur (clé)
-// Le contenu chiffré est enregistré sur le disque dans le fichier "cipheredA.txt"
-// Le pendant de ReadAllLines est WriteAllLines
 Console.Write("\n\nSPECIAL FEATURE: Clé de chiffrement [1-25]: ");
 byte key = Convert.ToByte(Console.ReadLine());
 
--- exos/diffit/README.md
+++ exos/diffit/README.md
@@ -8,10 +8,14 @@ Voici de quoi s’entraîner pour le test pratique
 Compléter les **TODO** de [Program.cs](Program.cs) en utilisant les fichiers examples [v1.txt](v1.txt) et [v2.txt](v2.txt).
 
 ### Démo
-[Télécharger la démo](https://eduvaud.sharepoint.com/:u:/s/ETML_INF_MAITRES_Teams/EYlYJ6FNaFNCtDvxyanH17UB6a5BeJWOjR5z0vkz-wpDrQ?e=Ph38Jh)
+- [Télécharger la démo autonome](https://eduvaud.sharepoint.com/:u:/s/ETML_INF_MAITRES_Teams/EYlYJ6FNaFNCtDvxyanH17UB6a5BeJWOjR5z0vkz-wpDrQ?e=Ph38Jh)
+
+OU
+
+- [Utiliser l’éxécutable dépendant du framework](diffit.exe)
 
 ## Aide
-- cheatsheet
+- [Cheatsheet](../../supports/linq-cheatsheet.pdf)
 - notes de cours
-- exercices précédents
+- [Exercices précédents](../../exos/words/)exercices précédents
 - dépôt local du cours
